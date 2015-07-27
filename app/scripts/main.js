713363a74120fc3e94e6e72c82fec3978b88ca4e
nelson119 <morphlina@gmail.com>
2015-07-28 02:02:50 +0800

    daily report

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,33 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+// console.log('\'Allo \'Allo!');
+$(function(){
+	// fit window size
+	(function(w){
+		$(w).on('scroll resize',function(){
+			var width = $(w).width();
+			var height = $(w).height();
+			$('body').width(width);
+			$('body').height(height);
+		}).trigger('resize');
+	}(window));
+
+	//mouse wheel dragon
+	$('.dragon').on('mousewheel',function(evt){
+		var direction = 1;
+
+		if(evt.deltaY < 0 ){
+			direction = -1;
+		}
+		if(+$(this).css('margin-left').replace(/px/,'') >= 0 && direction === 1)
+		{
+			TweenMax.to(this,.25,{
+				marginLeft :0
+			});
+			return false;
+		}
+		TweenMax.to(this,.3,{
+			marginLeft :'+=' + evt.deltaY * 8 + '%'
+		});
+		return false;
+	});
+});
