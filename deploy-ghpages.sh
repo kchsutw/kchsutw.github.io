aef26ff912abe1e0b4a9af32ccc6cdcc9b01ec27
nelson119 <morphlina@gmail.com>
2015-08-05 01:25:35 +0800

    travis encrypt

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+gulp build
 mkdir out && cd $_
 git init
 git config user.name nelson119
 git config user.email nelson119@outlook.com
-git remote add origin "https://28bf3f627806cd1a9d1fa5b064b87d8907bf2829:x-oauth-basic@github.com/kchsutw/kchsutw.github.io.git"
+git remote add origin $GH_REF
 cp -r ../dist/* .
 cp ../CNAME ./CNAME
 git add .
-git commit --quiet -m "Deployed to Github Pages"
+git commit -m "Deployed to Github Pages : 'date'"
 git push -f --quiet origin master:master
 cd ..
 rm -rf out || exit 0; 
