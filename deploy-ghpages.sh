e3eb835ff544762d1d662ceb6e8a5ca3d664ddeb
nelson119 <morphlina@gmail.com>
2015-08-05 01:30:41 +0800

    colorbox test

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-gulp build
-mkdir out && cd $_
-git init
-git config user.name nelson119
-git config user.email nelson119@outlook.com
-git remote add origin $GH_REF
-cp -r ../dist/* .
-cp ../CNAME ./CNAME
-git add .
-git commit -m "Deployed to Github Pages : 'date'"
-git push -f --quiet origin master:master
-cd ..
+
 rm -rf out || exit 0; 
