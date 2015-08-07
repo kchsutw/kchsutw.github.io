57652a0dc3d9122a2c04a312e3beedc2809d2f95
nelson119 <morphlina@gmail.com>
2015-08-07 22:20:56 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -46,13 +46,13 @@ gulp.task('html', ['styles'], function () {
 
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    // .pipe($.cache($.imagemin({
-    //   progressive: true,
-    //   interlaced: true,
-    //   // don't remove IDs from SVGs, they are often used
-    //   // as hooks for embedding and styling
-    //   svgoPlugins: [{cleanupIDs: false}]
-    // })))
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
     .pipe(gulp.dest('dist/images'));
 });
 
