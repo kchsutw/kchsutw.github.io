9d893957882952c4b1acb34d6d268b1cb03de16e
nelson <nelson119@outlook.com>
2015-08-22 02:52:16 +0800

    prefetch share meta

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -515,7 +515,7 @@ $(function(){
 			method: 'feed',
 			message:'#擇愛成家',
 			link: 'http://kchsu.com/?sn=' + serial,
-      		title = formData.name + ' 已經擇愛成家',
+      		title: formData.name + ' 已經擇愛成家',
 		  	description:'家 Ξ 是蜃樓海市　Ξ 是掠影浮光 Ξ 是我們寫進青春摺頁中的一撇一捺 Ξ 是想像也只能是想像 Ξ 昨天，我們仔細收藏 Ξ 今天，我們擇愛成家 〉',
 		  	picture:'http://api.kchsu.com/u/' + serial + '.png'
 		}, function(response){
